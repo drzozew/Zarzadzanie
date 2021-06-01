d6fa667d15d34983a7705dcf64817ab48ecc8c0e
Dzozew <jex.kara@gmail.com>
2021-06-01 18:30:56 +0200

    added argv options

--- emult_terminal.py
+++ emult_terminal.py
@@ -1,6 +1,9 @@
 from sys import argv
 from python_time.python_time_choises import Python_time_choises as PTC
-
+from datetime import date, timedelta
+from lib.db_functions import DB_Python_czas as DBP
+from lib.db_functions import DB_Niemiecki as DBN
+from lib.db_functions import DB_Drink as DBD
 
 if len(argv) > 1 and argv[1] == 'python_chart':
     x = PTC()
@@ -10,6 +13,46 @@ if len(argv) > 1 and argv[1] == 'python_chart':
 if len(argv) > 1 and argv[1] == 'python':
     x = PTC()
     x.choice_1_1()
+    exit()
+
+if len(argv) > 1 and argv[1] == '-p':
+    print(f"Dodane czas nauki Python z wczoraj {argv[2]}")
+    yesterday = date.today() - timedelta(days=1)
+    # adding data to Python DB
+    add_data = DBP()
+    add_data.add_data(yesterday, argv[2])
+    exit()
+
+if len(argv) > 1 and argv[1] == '-p0':
+    print("Dodane czas nauki Python z wczoraj 00:00:00")
+    yesterday = date.today() - timedelta(days=1)
+    # adding data to Python DB
+    add_data = DBP()
+    add_data.add_data(yesterday, '00:00:00')
+    exit()
+
+if len(argv) > 1 and argv[1] == '-n':
+    print(f"Dodane czas nauki Niemiecki z wczoraj {argv[2]}")
+    yesterday = date.today() - timedelta(days=1)
+    # adding data to Niemiecki DB
+    add_data = DBN()
+    add_data.add_data(yesterday, argv[2])
+    exit()
+
+if len(argv) > 1 and argv[1] == '-n0':
+    print(f"Dodane czas nauki Niemiecki z wczoraj 00:00:00")
+    yesterday = date.today() - timedelta(days=1)
+    # adding data to Niemiecki DB
+    add_data = DBN()
+    add_data.add_data(yesterday, '00:00:00')
+    exit()
+
+if len(argv) > 1 and argv[1] == '-d':
+    print(f"Dodane napoje z wczoraj kupiłem: {argv[2]} dosałem: {argv[3]}")
+    yesterday = date.today() - timedelta(days=1)
+    # adding data to drinks DB
+    add_data = DBD()
+    add_data.add_data(yesterday, argv[2], argv[3])
     exit()
 
 if len(argv) > 1:
--- terminal/terminal_argv.py
+++ terminal/terminal_argv.py
@@ -2,6 +2,5 @@ from sys import argv
 from src.data_base.data_base_functions import DataBase as DBF
 from src.menu.menu import Menu
 
-if len(argv) > 1 and argv[1] == 'setup':
-    print('Tworze baze danych')
-    db = DBF('SQLite3.db')
+if len(argv) > 1 and argv[1] == '-p':
+    print('Dodaje czas Python wczoraj')
